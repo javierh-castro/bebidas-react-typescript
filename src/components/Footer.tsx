3f602b372936ca6d05999e8f16d7328fc309908c
javierh-castro <javi2cc1@gmail.com>
2024-08-27 10:30:27 -0300

    fix: Correct minor errors detected in the final review

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function Footer() {
   return (
     <div className='flex justify-center items-center bg-slate-500 h-16 block font-semibold text-lg'>© 2024 Javier Castro Developer.</div>
